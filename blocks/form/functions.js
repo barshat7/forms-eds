f390726f5dcbf5be4dabfa1f6e739257a9b83fe1
Barshat Rai <barshatrai99@gmail.com>
2024-12-03 14:41:04 +0530

    format

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -4,7 +4,7 @@
  * @return {string}
  */
 function getGreetings() {
-    return 'Hello EDS';
+  return 'Hello EDS';
 }
 // eslint-disable-next-line import/prefer-default-export
 export { getGreetings };
