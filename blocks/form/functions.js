e626545ecda175ba5cac3f1dd28f2af6e9394f3b
Barshat Rai <barshatrai99@gmail.com>
2024-12-03 14:35:33 +0530

    custom functions

---
+++ blocks/form/functions.js
@@ -0,0 +1,11 @@
+/**
+ * Get Greetings
+ * @name getGreetings Concats first name and last name
+ * @return {string}
+ */
+function getGreetings() {
+    return "Hello EDS";
+}
+  
+// eslint-disable-next-line import/prefer-default-export
+export { getGreetings };
