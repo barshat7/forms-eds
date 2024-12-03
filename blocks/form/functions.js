665c982b42c6a2813561ace70573dd1249f1cc94
Barshat Rai <barshatrai99@gmail.com>
2024-12-03 14:37:56 +0530

    formatting

--- blocks/form/functions.js
+++ blocks/form/functions.js
@@ -1,11 +1,10 @@
 /**
  * Get Greetings
- * @name getGreetings Concats first name and last name
+ * @name getGreetings 
  * @return {string}
  */
 function getGreetings() {
-    return "Hello EDS";
+    return 'Hello EDS';
 }
-  
 // eslint-disable-next-line import/prefer-default-export
 export { getGreetings };
